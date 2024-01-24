7da29432e5c0c494c44d3d4dead05d31255f6363
wuhongqing <wuhongqing@bluemoon.com.cn>
2024-01-24 21:22:14 +0800

    feat: 0124

--- main-app/src/main.js
+++ main-app/src/main.js
@@ -1,18 +1,22 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
 import { registerMicroApps, start } from 'qiankun';
 
-const app = createApp(App);
+const app = createApp(App)
 
-// ...其他Vue相关配置
+app.use(createPinia())
+app.use(router)
 
-app.mount('#app');
-
-// 注册子应用
+// 在主应用中注册子应用
 registerMicroApps([
   { name: 'sub-app-1', entry: '//localhost:8081', container: '#subapp-viewport', activeRule: '/sub-app-1' },
-  { name: 'sub-app-2', entry: '//localhost:8082', container: '#subapp-viewport', activeRule: '/sub-app-2' },
 ]);
 
-// 启动主应用
-start();
+app.mount('#app')
+
+start()
